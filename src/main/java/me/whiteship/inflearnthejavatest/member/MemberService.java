ec3edcc628e11223f91bcfd41c43906e20f31f97
clarent <d.artagnan087@gmail.com>
2022-06-24 00:33:55 +0900

    Fix error

--- src/main/java/me/whiteship/inflearnthejavatest/member/MemberService.java
+++ src/main/java/me/whiteship/inflearnthejavatest/member/MemberService.java
@@ -5,8 +5,6 @@ import me.whiteship.inflearnthejavatest.domain.Member;
 import java.util.Optional;
 
 public interface MemberService {
-    // 이건 사용하지 않음
-    void validate(Long memberId) throws InvalidMemberException;
 
     Optional<Member> findById(Long memberId);
 }
--- src/test/java/me/whiteship/inflearnthejavatest/study/StudyServiceTest.java
+++ src/test/java/me/whiteship/inflearnthejavatest/study/StudyServiceTest.java
@@ -1,7 +1,6 @@
 package me.whiteship.inflearnthejavatest.study;
 
 import me.whiteship.inflearnthejavatest.domain.Member;
-import me.whiteship.inflearnthejavatest.member.InvalidMemberException;
 import me.whiteship.inflearnthejavatest.member.MemberService;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -17,6 +16,19 @@ import static org.mockito.Mockito.mock;
 class StudyServiceTest {
 
     /**
+     * mock annotation으로 코드를 줄임
+     *
+     * @ExtendWith(MockitoExtension.class) 이거 반드시 필요. @Mock 어노테이션 처리 해주는 Extension이 있어야해서
+     * 이렇게 test field로 만들어 두는 경우는 다른 Test method에서도 mock을 이용하는 경우.
+     * > 근데 짜피 Test class instance는 method마다 만들어 지므로.. 원래 class field를 만들어서 method에 사용하는
+     * > 그 의미와는 다름..
+     */
+    @Mock
+    MemberService memberService;
+    @Mock
+    StudyRepository studyRepository;
+
+    /**
      * 기본적인 mockito mock 원리
      */
     @Test
@@ -24,10 +36,6 @@ class StudyServiceTest {
 
         // 이렇게 interface를 만들어서 넣어주면 되는데.. 너무 번거로움
         MemberService memberService = new MemberService() {
-            @Override
-            public void validate(Long memberId) throws InvalidMemberException {
-
-            }
 
             @Override
             public Optional<Member> findById(Long memberId) {
@@ -44,18 +52,6 @@ class StudyServiceTest {
         assertNotNull(studyService); // null이면 error
     }
 
-    /**
-     * mock annotation으로 코드를 줄임
-     * @ExtendWith(MockitoExtension.class) 이거 반드시 필요. @Mock 어노테이션 처리 해주는 Extension이 있어야해서
-     * 이렇게 test field로 만들어 두는 경우는 다른 Test method에서도 mock을 이용하는 경우.
-     * > 근데 짜피 Test class instance는 method마다 만들어 지므로.. 원래 class field를 만들어서 method에 사용하는
-     * > 그 의미와는 다름..
-     */
-    @Mock
-    MemberService memberService;
-    @Mock
-    StudyRepository studyRepository;
-
     @Test
     void createStudyTest1() {
         StudyService studyService = new StudyService(memberService, studyRepository);
@@ -68,7 +64,7 @@ class StudyServiceTest {
      */
     @Test
     void createStudyService(@Mock MemberService memberService,
-                          @Mock StudyRepository studyRepository) {
+                            @Mock StudyRepository studyRepository) {
         StudyService studyService = new StudyService(memberService, studyRepository);
         assertNotNull(studyService); // null이면 error
     }
